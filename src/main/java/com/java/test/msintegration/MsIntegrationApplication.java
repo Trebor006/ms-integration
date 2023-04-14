c4d004ad2ff502bb94bfd49fe6e437f81585e5ce
Robert Cabrera <clrobert.06@gmail.com>
2023-04-14 00:09:29 -0400

    feat(master): remove lava flow

--- src/main/java/com/java/test/msintegration/MsIntegrationApplication.java
+++ src/main/java/com/java/test/msintegration/MsIntegrationApplication.java
@@ -1,7 +1,5 @@
 package com.java.test.msintegration;
 
-import com.java.test.msintegration.components.artemis.ArtemisProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MsIntegrationApplication {
 
-//  @Autowired private ArtemisProducer producer;
-
   public static void main(String[] args) {
     SpringApplication.run(MsIntegrationApplication.class, args);
   }
@@ -22,26 +18,4 @@ public class MsIntegrationApplication {
   public RestTemplate getresttemplate() {
     return new RestTemplate();
   }
-  //
-  //    @Autowired
-  //    private ArtemisListenerProcessor artemisListenerProcessor;
-  //
-  //    @PostConstruct
-  //    public void test() {
-  //
-  //        var messageType = MessageType.CONVERSION_REQUEST;
-  //        var conversionRequestDto = ConversionRequestDto.builder()
-  //                .amount(25000D)
-  //                .symbol("USD")
-  //                .currency("BTC")
-  //                .build();
-  //
-  //        var message = QueueMessage.builder()
-  //                .type(messageType)
-  //                .content(new Gson().toJson(conversionRequestDto))
-  //                .build();
-  //
-  //        producer.sendMessage(message);
-  ////        artemisListenerProcessor.process(new Gson().toJson(message));
-  //    }
 }
