d0a89525833582f2a8ef6f93447a1216da03b6fd
Robert Cabrera <clrobert.06@gmail.com>
2023-04-17 00:27:45 -0400

    feat(master): add tests

--- src/main/java/com/java/test/msintegration/services/response/ConversionResponseService.java
+++ src/main/java/com/java/test/msintegration/services/response/ConversionResponseService.java
@@ -1,11 +1,11 @@
 package com.java.test.msintegration.services.response;
 
 import com.google.gson.Gson;
+import com.java.test.msintegration.components.artemis.ProducerInterface;
 import com.java.test.msintegration.components.messages.MessageServiceInterface;
 import com.java.test.msintegration.dto.QueueMessage;
 import com.java.test.msintegration.entities.ConversionEntity;
 import com.java.test.msintegration.enums.MessageType;
-import com.java.test.msintegration.services.QueueSenderService;
 import com.java.test.msintegration.services.mapper.ConversionMapperServiceInterface;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,18 +19,19 @@ public class ConversionResponseService implements ConversionResponseServiceInter
 
   private final MessageServiceInterface messageService;
   private final ConversionMapperServiceInterface conversionMapperService;
-  private final QueueSenderService queueSenderService;
+  private final ProducerInterface producer;
 
   @Override
   @Async
   public void generateResponse(ConversionEntity conversion) {
     log.info(messageService.getMessage("process.getting.response"));
     var conversionResponseDto = conversionMapperService.mapToResult(conversion);
-    var message = QueueMessage.builder()
+    var message =
+        QueueMessage.builder()
             .type(MessageType.CONVERSION_RESPONSE)
             .content(new Gson().toJson(conversionResponseDto))
             .build();
 
-    queueSenderService.sendResponse(message);
+    producer.sendMessage(message);
   }
 }
